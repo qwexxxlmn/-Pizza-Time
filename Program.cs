699203277127ce41974eec5aa827496359e3ef5e
qwexxxlmn <alwaysoneafly@gmail.com>
2024-01-22 22:01:28 +0400

    Version_2.0

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace PizzaTime
+namespace Pizza_time
 {
     // Сущность "Пицца"
     public class Pizza
     {
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
     }
 
     // Сущность "Заказ"
@@ -36,7 +36,7 @@ namespace PizzaTime
         public void PickUpOrder(Pizzeria pizzeria, Order order)
         {
             pizzeria.NotifyUser(order);
-            Console.WriteLine($"{Name} забрал заказ. Приятного аппетита!");
+            Console.WriteLine($"{Name}забрал заказ. Приятного аппетита!");
         }
     }
 
@@ -63,8 +63,12 @@ namespace PizzaTime
 
         public void NotifyUser(Order order)
         {
-            Console.WriteLine($"Заказ {order.OrderNumber} готов. Пожалуйста, заберите его, {order.UserName}!");
+            Console.WriteLine($"Заказ {order.OrderNumber} готов. Пожалуйста, заберите его!");
             order.IsReady = true;
+
+            // Задержка перед тем, как пользователь заберет заказ
+            Console.WriteLine("Ожидаем, пока гость заберет заказ...");
+            Thread.Sleep(3000); // Задержка в миллисекундах (в данном случае 3 секунды)
         }
     }
 
@@ -74,13 +78,44 @@ namespace PizzaTime
         {
             // Пример использования
             Pizzeria pizzeria = new Pizzeria();
-            User user = new User { Name = "Иван" };
-            List<Pizza> pizzas = new List<Pizza> { new Pizza { Name = "Пепперони", Price = 10.99m } };
+            User user = new User { Name = "Гость " };
+
+            // Добавим несколько видов пицц для выбора пользователем
+            List<Pizza> menu = new List<Pizza>
+            {
+                new Pizza { Name = "1. Пепперони: ", Price = 549.00},
+                new Pizza { Name = "2. Маргарита: ", Price = 349.49},
+                new Pizza { Name = "3. Гавайская: ", Price = 799.99}
+            };
+
+            Console.WriteLine("Меню: ");
+            foreach (var pizza in menu)
+            {
+                Console.WriteLine($"{pizza.Name} - {pizza.Price:F2}");
+            }
+
+            Console.WriteLine("Выберите пиццу из меню (введите номер): ");
+            int pizzaChoice = int.Parse(Console.ReadLine());
+
+            if (pizzaChoice < 1 || pizzaChoice > menu.Count)
+            {
+                Console.WriteLine("Неверный выбор пиццы.");
+                Console.ReadLine();
+                return;
+            }
+
+            Pizza selectedPizza = menu[pizzaChoice - 1];
+
+            List<Pizza> pizzas = new List<Pizza> { selectedPizza };
 
             user.PlaceOrder(pizzeria, pizzas);
-            // Здесь могло бы быть ожидание оповещения о готовности заказа
+            Console.WriteLine($"Заказ на {selectedPizza.Name} успешно размещен. Номер заказа: {pizzeria.Orders.Last().OrderNumber}");
 
-            Order readyOrder = pizzeria.Orders[0];
+            // Задержка на приготовление пиццы в виде таймера
+            Console.WriteLine("Готовим пиццу...");
+            Thread.Sleep(5000); // Задержка в миллисекундах (в данном случае 5 секунд)
+
+            Order readyOrder = pizzeria.Orders.Last();
             user.PickUpOrder(pizzeria, readyOrder);
             Console.ReadLine();
         }
--- obj/Debug/net8.0/-Pizza-Time.AssemblyInfo.cs
+++ obj/Debug/net8.0/-Pizza-Time.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("-Pizza-Time")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c62b349d2451f1193d917bc8c886a34ae7c59c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788a06195d0843adb3075640c7dadf0077b7eaae")]
 [assembly: System.Reflection.AssemblyProductAttribute("-Pizza-Time")]
 [assembly: System.Reflection.AssemblyTitleAttribute("-Pizza-Time")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
